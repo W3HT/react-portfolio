9f4e5a555eba5c3c314130782896b94b43eef488
Frost <mpyang2021@gmail.com>
2021-11-19 16:39:41 -0600

    added title.js, did minor styling to navtabs

---
+++ src/components/Title.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Background 
+
+
+function Title() {
+    return (
+        <div className="text-warning bg-dark" style={{display: 'flex', justifyContent:'center', alignItems:'center'}}>
+            <h1 className="headTitle align-center">
+                Matthew P Yang
+            </h1>
+        </div>
+    )
+};
+
+export default Title;
