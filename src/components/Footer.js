2b221d5bcb41589e94cbf5b641ea4d2dacba7971
Frost <mpyang2021@gmail.com>
2021-11-19 16:55:02 -0600

    added footer.js, wireframe and imports on footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+// create footer with external pages
+
+// import react
+import React from 'react';
+// import fontaswesome icons
+import { faGitHub, FaLinkedin} from "react-icons/fa";
+// footer function
+function Footer() {
+
+// hovers
+    const handleMouseEnter = event => {
+        event.target.style.color = "######"
+    }
+    const handleMouseLeave = event => {
+        event.target.style.color = "######"
+    }
+
+// return code with jsx
+}
