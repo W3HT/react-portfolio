4caf03f7fb02e7da84fde2d4211f35269ae54216
Frost <mpyang2021@gmail.com>
2021-11-21 10:43:06 -0600

    updated Resume to include link to google doc

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,50 +1,40 @@
-import React from 'react';
-import DislplayPhoto from '../../images/profilePicture.jpg'
-import MyResume from '../../images/resume.png'
-
+import React from "react";
+import DislplayPhoto from "../../images/profilePicture.jpg";
+import MyResume from "../../images/resume.png";
 
 export default function Resume() {
   return (
     <div>
-      <div className="row mt-5  justify-content-center">
-        {/* <div className="col-sm-12 col-lg-5 ">
-          <div >
-            <img src={DislplayPhoto} alt="My Profile picuture"
-              style={{
-                width: "200px",
-                height: "auto",
-                display: "block",
-                marginLeft: "0",
-                marginRigth: "0",
-                border: "2px solid black",
-                boxShadow: "20px whitesmoke"}}
-              
-                />
-          </div>
-        </div> */}
-        
-        <div className="col-sm-12 col-lg-5">
-          <div >
-            <img src={MyResume} alt="My Resume picuture"
-              style={{
-                width: "auto",
-                height: "auto",
-                display: "block",
-                marginLeft: "auto",
-                marginRight: "auto",
-                marginBottom: "200px",
-                border: "2px solid black",
-                boxShadow: "10px whitesmoke"}}
-              
-                />
-          </div>
-          
-              
-              
+      <div className="mt-5">
+      <div className="row d-flex justifty-content-center">
+        <div className="col-sm-12 col-lg-12 text-center">
+          <a href="https://drive.google.com/file/d/1vVXl1YRGgRODMTg0nuJ7wfu144BEvox3/view?usp=sharing">
+            Download Expanded Resume Here
+          </a>
+        </div>
+      </div>
+        <div className="row d-flex justify-content-center">
+          <div className="col-sm-12 col-lg-12">
+            <div>
+              <img
+                src={MyResume}
+                alt="My Resume picuture"
+                style={{
+                  width: "auto",
+                  height: "auto",
+                  display: "block",
+                  marginLeft: "auto",
+                  marginRight: "auto",
+                  marginBottom: "200px",
+                  border: "2px solid black",
+                  boxShadow: "10px whitesmoke",
+                }}
+              />
+            </div>
           </div>
         </div>
-        </div>
-
-)}
-        
-
+      </div>
+      
+    </div>
+  );
+}
